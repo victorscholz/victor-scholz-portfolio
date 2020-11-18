8c1051df834868c7ed0c17dd8baeb0eac28b8f5e
victorscholz <vgscholz@gmail.com>
2020-11-18 16:12:10 -0500

    basic template, tweaking onHover to be more accurate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Canvas, useThree } from "react-three-fiber";
+import { HTML } from "drei";
+import { Block } from "./blocks";
+import { Shapes, Categories, Box } from "./Home";
+import state from "./store";
+import "./App.css";
+
+function HtmlContent({ className, style, children, portal }) {
+  const { size } = useThree();
+  return (
+    <HTML
+      portal={portal}
+      style={{
+        position: "absolute",
+        top: -size.height / 2,
+        left: -size.width / 2,
+        width: size.width,
+        height: size.height,
+      }}
+    >
+      <div className={className} style={style}>
+        {children}
+      </div>
+    </HTML>
+  );
+}
 
 function App() {
+  const [events, setEvents] = useState();
+  const domContent = useRef();
+  const scrollArea = useRef();
+  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        colorManagement
+        gl={{ alpha: false, antialias: true }}
+        camera={{ position: [0, 0, 4.5], fov: 50, near: 0.1, far: 100 }}
+        onCreated={({ gl, events }) => {
+          gl.setClearColor("white");
+          gl.toneMappingExposure = 2.5;
+          gl.toneMappingWhitePoint = 1;
+          // Export canvas events, we will put them onto the scroll area
+          setEvents(events);
+        }}
+      >
+        <Block factor={1.5} offset={0}>
+          <Shapes />
+          <HtmlContent portal={domContent}>
+            <div className="menu left" style={{ top: "2.55rem" }}>
+              <h2 style={{ fontSize: "2em", top: "4rem" }}>Victor Scholz</h2>
+            </div>
+            <div className="menu right">
+              {/* Add hyperlink onClick for each of these */}
+              <span>LinkedIn</span>
+              <span>Github</span>
+              <span>Medium</span>
+            </div>
+            <div className="jumbo">
+              <h1>
+                Full
+                <br />
+                Stack
+                <br />
+                Developer
+              </h1>
+              <Categories />
+            </div>
+          </HtmlContent>
+        </Block>
+
+        <Block factor={1.5} offset={1}>
+          <Box />
+          <HTML center portal={domContent}>
+            <h2>District Build NYC</h2>
+          </HTML>
+        </Block>
+
+        <Block factor={1.5} offset={2}>
+          <Box />
+          <HTML center portal={domContent}>
+            <h2>Churn</h2>
+          </HTML>
+        </Block>
+
+        <Block factor={-2} offset={4}>
+          <Box scale={[2, 2, 2]} />
+          <HTML center portal={domContent}>
+            <h2>Aeropress Dice</h2>
+          </HTML>
+        </Block>
+      </Canvas>
+
+      <div
+        className="scrollArea"
+        ref={scrollArea}
+        onScroll={onScroll}
+        {...events}
+      >
+        <div style={{ position: "sticky", top: 0 }} ref={domContent} />
+        <div style={{ height: `${state.pages * 100}vh` }} />
+      </div>
+    </>
   );
 }
 
